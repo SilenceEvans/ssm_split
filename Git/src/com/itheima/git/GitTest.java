50e8f5bb1310fc5a1f7a020a369c30a0919c3ea3
DixinWang@outlook.com <WZ199511>
2019-06-14 17:58:21 +0800

    加入isOdd方法

--- Git/src/com/itheima/git/GitTest.java
+++ Git/src/com/itheima/git/GitTest.java
@@ -2,6 +2,12 @@ package com.itheima.git;
 
 public class GitTest {
     public static void main(String[] args) {
+
         System.out.println("I Love Git!");
+
+    }
+
+    public boolean isOdd(int i){
+        return i%2 == 0;
     }
 }
